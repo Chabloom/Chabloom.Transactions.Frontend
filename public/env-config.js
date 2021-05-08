0b5cf61019cfd69bdfb297f73d6a3bac245038c1
Matthew Casey <mdcasey@chabloom.com>
2021-05-08 15:01:50 -0400

    Update gitignore and environment files

---
+++ public/env-config.js
@@ -0,0 +1,10 @@
+window._env_ = {
+  REACT_APP_ACCOUNTS_FRONTEND_ADDRESS: "https://localhost:3000",
+  REACT_APP_BILLING_FRONTEND_ADDRESS: "https://localhost:3001",
+  REACT_APP_ECOMMERCE_FRONTEND_ADDRESS: "https://localhost:3003",
+  REACT_APP_TRANSACTIONS_FRONTEND_ADDRESS: "https://localhost:3002",
+  REACT_APP_ACCOUNTS_BACKEND_ADDRESS: "https://accounts-api-dev-1.chabloom.com",
+  REACT_APP_BILLING_BACKEND_ADDRESS: "https://billing-api-dev-1.chabloom.com",
+  REACT_APP_ECOMMERCE_BACKEND_ADDRESS: "https://ecommerce-api-dev-1.chabloom.com",
+  REACT_APP_TRANSACTIONS_BACKEND_ADDRESS: "https://transactions-api-dev-1.chabloom.com",
+}
