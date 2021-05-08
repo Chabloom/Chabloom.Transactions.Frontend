1c9bcac1df9f662e67e55dd758c007af55a6a76a
Matthew Casey <mdcasey@chabloom.com>
2021-05-07 21:51:33 -0400

    Update docker build scripts

--- scripts/docker-build.ps1
+++ scripts/docker-build.ps1
@@ -1,6 +1,4 @@
-#!/bin/bash
-
 docker buildx create --name chabloom --platform linux/amd64,linux/arm64 --use
 
-timestamp=`date +%s`
+$timestamp=[int][double]::Parse((Get-Date -UFormat %s))
 docker buildx build -t mdcasey/chabloom-transactions-frontend:$timestamp -t mdcasey/chabloom-transactions-frontend:latest --push --platform linux/amd64,linux/arm64 .
