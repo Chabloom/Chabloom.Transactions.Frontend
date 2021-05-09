2dfaab90e2daa2d7aeada42721b5cbf1b5426a17
Matthew Casey <mdcasey@chabloom.com>
2021-05-08 22:02:22 -0400

    Use environment variable for redirect URL

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ import { UserManagerSettings } from "oidc-client";
 export const OidcConfiguration: UserManagerSettings = {
   authority: (window as any).__env__.REACT_APP_ACCOUNTS_BACKEND_ADDRESS,
   client_id: "Chabloom.Transactions.Frontend",
-  redirect_uri: `${process.env.PUBLIC_URL}/signin-oidc`,
-  post_logout_redirect_uri: `${process.env.PUBLIC_URL}/signout-oidc`,
+  redirect_uri: `${(window as any).__env__.REACT_APP_TRANSACTIONS_FRONTEND_ADDRESS}/signin-oidc`,
+  post_logout_redirect_uri: `${(window as any).__env__.REACT_APP_TRANSACTIONS_FRONTEND_ADDRESS}/signout-oidc`,
   response_type: "code",
   scope: "openid profile Chabloom.Transactions.Backend",
   filterProtocolClaims: true,
